a51ad739bac3179d639b63a4ee9764436c25f2a9
RJay <sharma.rajan3097@gmail.com>
2020-02-14 17:17:35 +0530

    Third Commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -13,9 +14,16 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   }
 
+  heroes = HEROES;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }

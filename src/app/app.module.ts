2d70fe95d0460df8c9b5fafe8bf4e43d90cc7334
RJay <sharma.rajan3097@gmail.com>
2020-02-14 11:00:31 +0530

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
